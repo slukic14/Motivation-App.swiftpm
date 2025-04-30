ddb6f93ab1e3547cbd1deee1c22dc4186ea983a2
slukic14 <slukic6255@stu.d214.org>
2025-04-30 15:05:14 -0500

    Added app design

--- ContentView.swift
+++ ContentView.swift
@@ -3,7 +3,42 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Test")
+            Text("Motivation For Today")
+                .font(.title)
+                .offset(CGSize(width: 0.0, height: 20.0))
+            ZStack {
+                Rectangle()
+                    .size(CGSize(width: 170.0, height: 170.0))
+                    .offset(CGSize(width: 10.0, height: 100.0))
+                    .opacity(0.4)
+                
+                Rectangle()
+                    .size(CGSize(width: 170.0, height: 170.0))
+                    .offset(CGSize(width: 210.0, height: 100.0))
+                    .opacity(0.4)
+                
+                Rectangle()
+                    .size(CGSize(width: 170.0, height: 170.0))
+                    .offset(CGSize(width: 10.0, height: 300.0))
+                    .opacity(0.4)
+                
+                Rectangle()
+                    .size(CGSize(width: 170.0, height: 170.0))
+                    .offset(CGSize(width: 210.0, height: 300.0))
+                    .opacity(0.4)
+                
+                Rectangle()
+                    .size(CGSize(width: 170.0, height: 170.0))
+                    .offset(CGSize(width: 10.0, height: 500.0))
+                    .opacity(0.4)
+                
+                Rectangle()
+                    .size(CGSize(width: 170.0, height: 170.0))
+                    .offset(CGSize(width: 210.0, height: 500.0))
+                    .opacity(0.4)
+               
+               
+            }
            
         }
     }
