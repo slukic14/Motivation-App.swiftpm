420cd2b4ac27b1a9f8465f162fb58418d6f6f3ff
slukic14 <slukic6255@stu.d214.org>
2025-04-30 14:45:28 -0500

    Added quotes and explanations in a new view

--- ContentView.swift
+++ ContentView.swift
@@ -3,17 +3,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            let quote1 = "Attitude is a little thing that makes a big difference."
-            let quote2 = "Don't sit down and wait for the opportunities to come. Get up and make them."
-            let quote3 = "You are never too old to set another goal or to dream a new dream."
-            let quote4 = "It is during our darkest moments that we must focus to see the light."
-            let quote5 = "All dreams are within reach. All you have to do is keep moving towards them."
-            let quote6 = "It is never too late to be what you might have been."
-            let quote7 = "It always seems impossible until it's done."
-            let quote8 = "When it comes to luck, you make your own."
-            let quote9 = "Your story is what you have, what you will always have. It is something to own."
-            let quote10 = "A problem is a chance for you to do your best."
-            
+            Text("Test")
            
         }
     }
